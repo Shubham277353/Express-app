6dd9ec1c5fbfc4be39f7f0b94d5cd35a25ab622d
Shubham <golubhandari405@gmail.com>
2026-07-14 16:41:25 +0000

    learn ejs and how to render in server side and static renders as well

--- app.js
+++ app.js
@@ -8,6 +8,32 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+const details = {
+  pNo: 423421453,
+  email: "starboit@gmail.com",
+  address: "street-2, chinatown, China",
+}
+
+const message = "Hello its your boi ksi olejilebitit my guy"
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.get('/about', (req,res)=> {
+  res.render("about", {message: message, links: links, details: details });
+})
+
+app.get("/", (req, res) => {
+  res.render("index", { links: links, users: users, details: details });
+});
+
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
